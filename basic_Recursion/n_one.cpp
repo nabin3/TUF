ac8b95404bd55feec95dfa6169d4f2aeafbe4c18
nabin3 <103164227+nabin3@users.noreply.github.com>
2023-01-24 08:54:42 +0530

    mini9

--- basic_Recursion/n_one.cpp
+++ basic_Recursion/n_one.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 void ntoOne(int i) {
@@ -12,4 +12,23 @@ int main() {
 	cout << "From which number you want to print to 0: ";
 	cin >> n;
 	ntoOne(n);
+}*/
+
+
+// using backtracking
+
+#include<iostream>
+using namespace std;
+
+void ntoOne(int i, int n) {
+	if (i > n) return;
+	ntoOne(i + 1, n);
+	cout << i << " ";
+}
+int main() {
+	int n;
+	cout << "Enter the number from which you want to print until0 ";
+	cin >> n;
+	ntoOne(0, n);
+	cout << endl;
 }
